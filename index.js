4b2f27860696e45086086e3ffb2dcb7f8a6032bc
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2023-02-08 23:12:05 -0300

    fixed PORT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from "./src/app.js";
-const PORT = process.env.POST || 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`server running on: localhost:${PORT}` );
