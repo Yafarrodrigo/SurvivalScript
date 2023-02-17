792b6dc8c01bd9fa0df2182b7466c950ab4fee41
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2023-02-17 07:24:37 -0300

    test railway

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import app from "./src/app.js";
 const PORT = process.env.PORT || 3000
-import os from 'os'
+//import os from 'os'
 
-const networkInterfaces = os.networkInterfaces();
+//const networkInterfaces = os.networkInterfaces();
 
 app.listen(PORT, () => {
-    console.log(`server running on: ${networkInterfaces['Ethernet'][0].address}:${PORT}` );
+    //console.log(`server running on: ${networkInterfaces['Ethernet'][0].address}:${PORT}` );
+    console.log(`server running on: ip:${PORT}` );
 })
